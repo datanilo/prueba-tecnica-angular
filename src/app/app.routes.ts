14ad33d64a33d8b24851744d19a9c3f09d1a0a86
datanilo <bd.mendez10@uniandes.edu.co>
2025-04-22 07:00:00 -0500

    Definición de rutas de la aplicación
    Ruta principal para la página de inicio
    Ruta de búsqueda con protección de autenticación
    Rutas para detalles de artistas, álbumes y playlists
    Rutas para callback de autenticación OAuth

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ArtistDetailComponent } from './pages/artist-detail/artist-detail.component';
+import { AlbumDetailComponent } from './pages/album-detail/album-detail.component';
+import { PlaylistsComponent } from './pages/playlists/playlists.component';
+import { PlaylistDetailComponent } from './pages/playlist-detail/playlist-detail.component';
+import { AuthCallbackComponent } from './pages/auth-callback/auth-callback.component';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent, canActivate: [authGuard] },
+  { path: 'artist/:id', component: ArtistDetailComponent, canActivate: [authGuard] },
+  { path: 'album/:id', component: AlbumDetailComponent, canActivate: [authGuard] },
+  { path: 'playlists', component: PlaylistsComponent, canActivate: [authGuard] },
+  { path: 'playlist/:id', component: PlaylistDetailComponent, canActivate: [authGuard] },
+  { path: 'auth/callback', component: AuthCallbackComponent },
+  { path: '**', redirectTo: '' }
+];
